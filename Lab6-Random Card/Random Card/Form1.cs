b5987074e15950c89f5c93e13d481aa047147114
gabgab017 <121985892+gabgab017@users.noreply.github.com>
2023-06-07 11:26:29 -0700

    Random cards

---
+++ Lab6-Random Card/Random Card/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getCardButton_Click(object sender, EventArgs e)
+        {
+            // TODO
+            // Create a Random object.
+            Random rand = new Random();
+
+            // Get a random index.
+            int x = rand.Next(0, 54);
+
+            // Display a card.
+            cardPictureBox.Image = cardImageList.Images[x];
+        }
+
+    }
+}
