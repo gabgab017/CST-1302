545f97242efb27537b3e0fb134d724bd8e13350c
gabgab017 <121985892+gabgab017@users.noreply.github.com>
2023-06-13 20:47:14 -0700

    Assignment1-Exercise1

---
+++ Assignment1-Exercise1/Assignment1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(60, 123);
+            button1.Name = "button1";
+            button1.Size = new Size(227, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Generate random numbers";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(308, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(308, 15);
+            label1.TabIndex = 2;
+            label1.Text = "How many random numbers would you like to generate?";
+            label1.Click += label1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(361, 216);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Random number generator";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(389, 244);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+    }
+}
---
+++ Assignment1-Exercise1/Assignment1/Form1.cs
@@ -0,0 +1,50 @@
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            saveFileDialog.Title = "Save Random Numbers";
+            DialogResult dialogResult = saveFileDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+
+                int count;
+                if (!int.TryParse(textBox1.Text, out count) || count < 1)
+                {
+                    MessageBox.Show("Invalid input. Please enter a positive integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Random random = new Random();
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        int randomNumber = random.Next(1, 101);
+                        writer.WriteLine(randomNumber);
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
