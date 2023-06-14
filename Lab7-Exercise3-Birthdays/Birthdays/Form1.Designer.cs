3aa7c7502764a30af369ddf7655b399b541687e9
gabgab017 <121985892+gabgab017@users.noreply.github.com>
2023-06-14 11:47:34 -0700

    Lab7-Exercise3-Birthdays

---
+++ Lab7-Exercise3-Birthdays/Birthdays/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Birthdays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.namesListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.birthdayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // namesListBox
+            // 
+            this.namesListBox.FormattingEnabled = true;
+            this.namesListBox.Location = new System.Drawing.Point(24, 48);
+            this.namesListBox.Name = "namesListBox";
+            this.namesListBox.Size = new System.Drawing.Size(174, 95);
+            this.namesListBox.TabIndex = 0;
+            this.namesListBox.SelectedIndexChanged += new System.EventHandler(this.namesListBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select a Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Birthday:";
+            // 
+            // birthdayLabel
+            // 
+            this.birthdayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.birthdayLabel.Location = new System.Drawing.Point(88, 158);
+            this.birthdayLabel.Name = "birthdayLabel";
+            this.birthdayLabel.Size = new System.Drawing.Size(100, 23);
+            this.birthdayLabel.TabIndex = 3;
+            this.birthdayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.birthdayLabel.Click += new System.EventHandler(this.birthdayLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 208);
+            this.Controls.Add(this.birthdayLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namesListBox);
+            this.Name = "Form1";
+            this.Text = "Birthdays";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox namesListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label birthdayLabel;
+    }
+}
+
---
+++ Lab7-Exercise3-Birthdays/Birthdays/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Birthdays
+{
+    public partial class Form1 : Form
+    {
+        // Dictionary with names and birthdays
+        Dictionary<string, string> birthdays =
+            new Dictionary<string, string>()
+            {
+                {"Cameron", "4/17/1998" },
+                {"Kathryn", "5/7/1996"  },
+                {"Jason",   "1/22/1997" },
+                {"Lola",    "5/5/1995"  },
+                {"Wesley",  "10/4/1999" }
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Display the names in the ListBox.
+            foreach (string name in birthdays.Keys)
+            {
+                namesListBox.Items.Add(name);
+            }
+        }
+
+        private void namesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // TODO
+
+            // Get the selected name.
+            string selectedName = namesListBox.SelectedItem.ToString();
+
+            // Get the selected person's birthday.
+            birthdayLabel.Text = birthdays[selectedName];
+        }
+
+        private void birthdayLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
