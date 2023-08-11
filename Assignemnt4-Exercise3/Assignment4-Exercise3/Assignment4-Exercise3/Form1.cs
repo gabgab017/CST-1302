84176d7a7ad0bc319bd601d1716dc3f531413506
gabgab017 <121985892+gabgab017@users.noreply.github.com>
2023-08-11 15:12:30 -0700

    Assignment4-Exercise3

---
+++ Assignemnt4-Exercise3/Assignment4-Exercise3/Assignment4-Exercise3/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment4_Exercise3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personnelDataSet.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter.Fill(this.personnelDataSet.Employee);
+
+        }
+
+        private void employeeBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.employeeBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personnelDataSet);
+
+        }
+    }
+}
---
+++ Assignemnt4-Exercise3/Assignment4-Exercise3/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Employee VALUES (1, 'John Doe', 'Manager', 30.5);
+INSERT INTO Employee VALUES (2, 'Jane Smith', 'Developer', 28.5);
+INSERT INTO Employee VALUES (3, 'Alice Johnson', 'Designer', 27.0);
+INSERT INTO Employee VALUES (4, 'Bob Brown', 'Tester', 25.5);
+INSERT INTO Employee VALUES (5, 'Charlie White', 'Admin', 22.5);
